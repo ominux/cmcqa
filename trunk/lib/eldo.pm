900499b6652e6abb1c3028ff9089463b40db3d27
gjcoram <gjcoram@28fdd317-644f-0410-a39b-517fd7010229>
2008-06-11 21:24:38 +0000

    m-factor fixes for noise of floating terminal

--- trunk/lib/eldo.pm
+++ trunk/lib/eldo.pm
@@ -543,8 +543,17 @@ sub generateCommonNetlistInfo {
     foreach $pin (@main::Pin) {push(@Pin_x,"${pin}_x")}
     print OF ".subckt mysub ".join(" ",@Pin_x);
     foreach $pin (@main::Pin) {
-        if ($main::isFloatingPin{$pin}) { # assumed "dt" thermal pin, no scaling sign change
-            print OF "v_$pin ${pin} ${pin}_x 0";
+        if ($main::isFloatingPin{$pin}) {
+            if ($main::outputNoise && $pin eq $main::Outputs[0]) {
+                if ($variant=~/^m$/) {
+                    $eFactor=sqrt($main::mFactor);
+                } else {
+                    $eFactor=1;
+                }
+                print OF "e_$pin ${pin}_x 0 ${pin} 0 $eFactor";
+            } else { # assumed "dt" thermal pin, no scaling sign change
+                print OF "v_$pin ${pin} ${pin}_x 0";
+            }
         } elsif ($variant=~/^Flip/ && defined($main::flipPin{$pin})) {
             print OF "e_$pin ${pin}_v 0 $main::flipPin{$pin}_x 0 $eFactor";
             print OF "v_$pin ${pin}_v ${pin} 0";
